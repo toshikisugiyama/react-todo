d38fc93decde2628611ea15cf261313f27e3b809
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-10-03 10:46:15 +0900

    implement filter

---
+++ src/components/Filter.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class Filter extends React.Component{
+  render(){
+    const {filter} = this.props
+    return(
+      <select value={filter} onChange={this.handleChange}>
+        <option value="all">すべて</option>
+        <option value="uncompleted">未完了</option>
+        <option value="completed">完了済み</option>
+      </select>
+    )
+  }
+  handleChange = e => {
+    this.props.onChange(e.currentTarget.value)
+
+  }
+}
+
+export default Filter
